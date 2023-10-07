c851f20e942446cb029bfcba43f8fc530d3d8d33
Bot & Web Scraping Expert <128396591+thsWebstorm03@users.noreply.github.com>
2023-10-08 07:35:30 +0800

    update router

--- router.js
+++ router.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const pointsRouter = require('./routes/pointsRouter');
 const tongueRouter = require('./routes/tongueRouter');
 
+router.get('/', (req, res) => {
+   return res.send("Hello world");
+})
 router.use('/api/points', pointsRouter);
 router.use('/api/tongue', tongueRouter);
 
